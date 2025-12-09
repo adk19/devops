aff618befbf269e625f207adfd9f949e15c83a6d
adk19 <ajayd.keypress@gmail.com>
2025-12-09 16:56:15 +0530

    Commit All code

--- eslint.config.js
+++ eslint.config.js
@@ -3,34 +3,48 @@ const prettierPlugin = require("eslint-plugin-prettier");
 
 module.exports = [
     js.configs.recommended,
+
     {
         files: ["**/*.js"],
+
         languageOptions: {
             ecmaVersion: 2021,
             sourceType: "commonjs",
+
             globals: {
-                module: "readonly",
-                require: "readonly",
+                console: "readonly",
                 process: "readonly",
+                setTimeout: "readonly",
+                clearTimeout: "readonly",
+                Buffer: "readonly",
                 __dirname: "readonly",
                 __filename: "readonly",
+                module: "readonly",
+                require: "readonly",
+
                 describe: "readonly",
                 it: "readonly",
                 test: "readonly",
                 expect: "readonly",
+                beforeAll: "readonly",
+                afterAll: "readonly",
+                beforeEach: "readonly",
+                afterEach: "readonly",
                 jest: "readonly"
             }
         },
+
         plugins: {
             prettier: prettierPlugin
         },
+
         rules: {
             "prettier/prettier": "error",
-            "no-unused-vars": "warn",
             "no-console": "off",
+            "no-unused-vars": "warn",
             "no-undef": "error",
-            semi: ["error", "always"],
             quotes: ["error", "double"],
+            semi: ["error", "always"]
         }
     }
 ];
